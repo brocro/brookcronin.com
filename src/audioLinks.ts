d39fad301904c352b1d587868f330fc7670fd18d
Brook Cronin <brook.cronin@qt.io>
2024-09-20 09:02:49 +0200

    add new track

--- src/audioLinks.ts
+++ src/audioLinks.ts
@@ -1,14 +1,18 @@
 export const audioFiles = [
     {
-      name: 'Late Again',
-      url: 'https://media.githubusercontent.com/media/brocro/audio-storage/main/audio/LateAgain.mp3'
+        name: 'Late Again',
+        url: 'https://media.githubusercontent.com/media/brocro/audio-storage/main/audio/LateAgain.mp3'
     },
     {
-      name: 'Under My Nails',
-      url: 'https://media.githubusercontent.com/media/brocro/audio-storage/main/audio/UnderMyNails.mp3'
+        name: 'Under My Nails',
+        url: 'https://media.githubusercontent.com/media/brocro/audio-storage/main/audio/UnderMyNails.mp3'
     },
     {
         name: 'Heartfelt',
         url: 'https://media.githubusercontent.com/media/brocro/audio-storage/main/audio/Heartfelt.mp3'
     },
-  ];
+    {
+        name: 'Melancholiian',
+        url: 'https://media.githubusercontent.com/media/brocro/audio-storage/main/audio/Melancholiian.mp3'
+    },
+];
